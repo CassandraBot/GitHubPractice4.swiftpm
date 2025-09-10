c0ef927e86cf98172bde013444beea6e00880489
Lily <lmakula6243@stu.d214.org>
2025-09-10 11:18:09 -0500

    I added the headerview to the top of the contentview.

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,9 @@
 import SwiftUI
-
 struct ContentView: View {
     @State var tasks: [Task] = [Task(nameOfTask: "Mow the lawn"), Task(nameOfTask: "Feed the dog"),
                                 Task(nameOfTask: "Grocery Shop") ]
     var body: some View {
+        HeaderView(tasks: $tasks)
         VStack {
             List {
                 ForEach(tasks, id: \.self){
