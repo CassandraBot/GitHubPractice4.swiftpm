9e1a6bbfd0d9c946d559467aa78ae0cd5ca032f7
Cassandra <cbotnari8050@stu.d214.org>
2025-09-10 11:02:34 -0500

    made headerView, Binded, enteredText, buttons and appending

---
+++ HeaderView.swift
@@ -0,0 +1,27 @@
+//
+//  HeaderView.swift
+//  GitHubPractice4
+//
+//  Created by Cassandra Botnari on 9/10/25.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    @Binding var tasks: [Task]
+    @State var enteredText: String = ""
+    var body: some View {
+        HStack {
+            TextField("Add a new task", text: $enteredText)
+            Button{
+                var enteredTextTask = Task(nameOfTask: enteredText)
+                tasks.append(enteredTextTask)
+                enteredText = ""
+            } label:{
+                Text("Add")
+            }
+            
+        }
+    }
+}
+
